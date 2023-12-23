0c6db0553342a4e3de6ab503e9479d91f2837f5e
Milo4kaa <105012752+Milo4kaa@users.noreply.github.com>
2023-12-23 11:39:19 +0300

    Изменения в 152_Nim-SolitaireV2.py

--- 1_Block/15/152_Nim-SolitaireV2.py
+++ 1_Block/15/152_Nim-SolitaireV2.py
@@ -1,35 +1,31 @@
-test_string = '''
-10
-1
-1
-5
-3
-2
-4
-4
-3
-'''[1:-1].split('\n')[::-1]
 
-def input(prompt=''):
-    if prompt:
-        print(prompt)
-    tmp = test_string.pop()
-    print(tmp)
-    return tmp
-
-def play_nim():
+def play_nim(stones):
     print("Игра Ним")
-    stones = int(input("Введите количество камней в куче: "))
+    print("Количество камней в куче:", stones)
     while stones > 0:
-        player_choice = int(input("Сколько камней хотите взять (от 1 до 3)? "))
-        if player_choice < 1 or player_choice > 3:
-            print("Некорректный ход! Введите число от 1 до 3.")
-            continue
-        if player_choice > stones:
-            print("Некорректный ход! Введите число не больше оставшегося количества камней.")
-            continue
-        stones -= player_choice
+        if stones % 4 == 0:
+            computer_choice = 3
+        else:
+            computer_choice = stones % 4
+        print("Компьютер взял", computer_choice, "камней.")
+        stones -= computer_choice
         print("В куче осталось", stones, "камней.")
-    print("Игра окончена. Вы победили!")
+        if stones <= 0:
+            print("Компьютер победил!")
+            break
 
-play_nim()
+        if stones > 0:
+            if stones % 4 == 0:
+                player_choice = 1
+            else:
+                player_choice = stones % 4
+            print("Игрок взял", player_choice, "камней.")
+            stones -= player_choice
+            print("В куче осталось", stones, "камней.")
+            if stones <= 0:
+                print("Вы победили!")
+                break
+
+
+for stones in range(1, 30):
+    play_nim(stones)
