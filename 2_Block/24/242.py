d7cf34b156b002e440c2c9a13655fa3d476d9c98
Milo4kaa <105012752+Milo4kaa@users.noreply.github.com>
2024-02-24 11:26:53 +0300

    Delete 2_Block/24/242.py

--- 2_Block/24/242.py
+++
@@ -1,9 +0,0 @@
-list = list()
-number = int(input('Введите количество строк: '))
-for i in range(number):
-    str = input('Введите строку: ')
-    list.append(str)
-find = input('Введите поисковую строку: ')
-for i in list:
-    if find in i:
-        print(i)
