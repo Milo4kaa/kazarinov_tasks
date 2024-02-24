5ad47780244743efd0f1ce7a46e9bc13103a467f
Milo4kaa <105012752+Milo4kaa@users.noreply.github.com>
2024-02-24 11:25:52 +0300

    Delete 2_Block/23/232.py

--- 2_Block/23/232.py
+++
@@ -1,8 +0,0 @@
-number = int(input('Введите размер доски: '))
-symb = 'ABCDEFGH'
-for i in range(number, 0, -1):
-    for j in symb[0:number]:
-        print(j,i,sep="",end=" ")
-    if j == symb[number-1]:
-        print()
-
