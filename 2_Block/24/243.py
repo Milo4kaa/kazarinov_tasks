b711eb0a8b9669b695848f7b750218f865df8c82
Milo4kaa <105012752+Milo4kaa@users.noreply.github.com>
2024-02-24 11:27:00 +0300

    Delete 2_Block/24/243.py

--- 2_Block/24/243.py
+++
@@ -1,9 +0,0 @@
-nums = list()
-z = 0
-number = int(input('Введите количество строк: '))
-for i in range(number):
-    num = int(input('Введите число: '))
-    nums.append(num)
-for i in range(number-1):
-    print(nums[z]+nums[z+1])
-    z += 1
