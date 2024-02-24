60a3e0f484b4e22965f35e1f55c5b75460f4d79b
Milo4kaa <105012752+Milo4kaa@users.noreply.github.com>
2024-02-24 11:26:47 +0300

    Delete 2_Block/24/241.py

--- 2_Block/24/241.py
+++
@@ -1,7 +0,0 @@
-basket = list()
-number = int(input('Введите количество покупок: '))
-for i in range(number):
-    buy = input('Введите товар для добавления в корзину: ')
-    basket.append(buy)
-for i in basket:
-    print(i)
